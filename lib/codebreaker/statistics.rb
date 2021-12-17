b16fc7e42c526419fff180197642f43c2e69c818
yaroslavkarpenko <yaroslavkarpenko1505@gmail.com>
2021-12-17 05:51:45 +0200

    added a separate class that generates statistics

---
+++ lib/codebreaker/statistics.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'autoloader'
+
+module Codebreaker
+  class Statistics
+    include FileStore
+    def show_statistics
+      load_file&.each&.sort_by { |game| [game[:available_attempts], game[:used_hints], game[:used_attempts]] }
+    end
+  end
+end
