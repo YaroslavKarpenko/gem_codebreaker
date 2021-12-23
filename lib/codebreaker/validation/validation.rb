1e32d5550150f5d295255c6e45a84397d045fc27
yaroslavkarpenko <yaroslavkarpenko1505@gmail.com>
2021-12-23 19:16:04 +0200

    wrapped validation module

--- lib/codebreaker/validation/validation.rb
+++ lib/codebreaker/validation/validation.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 require_relative 'errors'
+module Codebreaker
+  module Validation
+    def name_validator(name)
+      raise LengthError unless (3..20).cover?(name.length)
+    end
 
-module Validation
-  def name_validator(name)
-    raise LengthError unless (3..20).cover?(name.length)
-  end
-
-  def guess_validator(guess)
-    raise InputError unless guess.join.match(/^[1-6]{4}$/)
+    def guess_validator(guess)
+      raise InputError unless guess.join.match(/^[1-6]{4}$/)
+    end
   end
 end
